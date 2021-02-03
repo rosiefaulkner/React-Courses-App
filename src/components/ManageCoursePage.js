a6342ab193b217604a9740912ffcb9a3928eb469
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2021-02-02 23:09:19 -0500

    creates form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import {Prompt} from 'react-router-dom';
+import CourseForm from './CourseForm';
 
 const ManageCoursePage = props => {
     return (
         <>
             <h2>Manage Course</h2>
-            {props.match.params.slug}
+            <CourseForm />
         </>
     )
 }
