5d95863aaedc33857a389037831d327d6f228386
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2021-02-02 22:50:51 -0500

    adds url parameters for individual courses in course list

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
