aea7da82af324e4f0421b164f7c5a53f6420c64f
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2021-02-04 00:42:07 -0500

    populating form via url and input validation and proptypes

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //import {Prompt} from 'react-router-dom';
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -12,6 +13,13 @@ const ManageCoursePage = (props) => {
     authorId: null,
     category: "",
   });
+
+  useEffect(() => {
+    const slug = props.match.params.slug; //from the path `/courses/:slug`
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course))
+    }
+  }, [props.match.params.slug]);
 
   function handleChange({ target }) {
     const updatedCourse = {
@@ -21,11 +29,24 @@ const ManageCoursePage = (props) => {
     setCourse(updatedCourse);
   }
 
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is Required.";
+    if (!course.authorId) _errors.authorId = "Author ID is Required.";
+    if (!course.category) _errors.category = "Category is Required.";
+
+    setErrors(_errors);
+    // Form is valid if the errors object has no properties
+    return Object.keys(_errors).length === 0;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     courseApi.saveCourse(course).then(() => {
-        props.history.push("/courses");
-        toast.success(' Course saved.')
+      props.history.push("/courses");
+      toast.success(" Course saved.");
     });
   }
 
@@ -33,6 +54,7 @@ const ManageCoursePage = (props) => {
     <>
       <h2>Manage Course</h2>
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
