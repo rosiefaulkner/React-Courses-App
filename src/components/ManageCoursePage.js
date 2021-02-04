66a661067f11651320a2202af991650d10d2af5e
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2021-02-03 22:58:32 -0500

    declaring state change handlers

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,27 +1,30 @@
-import React, {useState}  from 'react';
+import React, { useState } from "react";
 //import {Prompt} from 'react-router-dom';
-import CourseForm from './CourseForm';
+import CourseForm from "./CourseForm";
 
-const ManageCoursePage = props => {
-    const [course, setCourse] = useState({
-        id: null,
-        slug: "",
-        title: "",
-        authorId: null, 
-        category: ""
-    })
+const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
 
-    function handleChange(event) {
-        const updatedCourse = {...course, [event.target.name]: event.target.value};
-        setCourse(updatedCourse);
-    }
+  function handleChange({ target }) {
+    const updatedCourse = {
+      ...course,
+      [target.name]: target.value,
+    };
+    setCourse(updatedCourse);
+  }
 
-    return (
-        <>
-            <h2>Manage Course</h2>
-            <CourseForm course={course} onTitleChange={handleChange} />
-        </>
-    )
-}
+  return (
+    <>
+      <h2>Manage Course</h2>
+      <CourseForm course={course} onTitleChange={handleChange} />
+    </>
+  );
+};
 
 export default ManageCoursePage;
